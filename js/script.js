ce65030934be76afc282e06eedda1b8889005672
maicol <maicol.matteoli@gmail.com>
2021-06-01 14:45:21 +0200

    add: es 2

--- js/script.js
+++ js/script.js
@@ -18,3 +18,26 @@ else if(str1.length < str2.length){
         console.log("le due parole : " + str1 +" e  " + str2 + "hanno la stessa lunghezza");
         document.getElementById("output").innerHTML="le due parole : " + str1 +" e " + str2 + " hanno la stessa lunghezza";
     }
+
+
+
+//---------------ES 2------------------------
+
+var numbers = [];
+for(let i=0 ; i<6 ; i++){
+    numbers[i]=Math.floor(prompt("inserisci un numero : "));
+}
+console.log(numbers);
+
+var numero=0;
+var somma=0;
+var i=0;
+
+for(i=0 ; i<5 ; i++){
+    numero = Math.floor(prompt("inserisci un numero : "));
+    somma += numero;
+    if(numero % 2 == 0){
+        document.getElementById("pari").innerHTML += " "+ numero + " ";
+    }
+}
+document.getElementById("output").innerHTML= "la somma è : " + somma;
