39240f0ddc2640938d5093ad5e1da6327f6c3c9d
maicol <maicol.matteoli@gmail.com>
2021-06-01 14:49:31 +0200

    es 2 fix visualizzazione

--- js/script.js
+++ js/script.js
@@ -1,39 +1,38 @@
 //--------------ES1 -------------------
-var str1 = prompt("inserisci una parola : ");
+// var str1 = prompt("inserisci una parola : ");
 
-var str2 = prompt("inseriscine un'altra :  ");
+// var str2 = prompt("inseriscine un'altra :  ");
 
-if(str1.length > str2.length)
-{
-    console.log("la parola piu corta è : " + str2);
-    document.getElementById("output").innerHTML= "la parola più corta è : " + str2;
+// if(str1.length > str2.length)
+// {
+//     console.log("la parola piu corta è : " + str2);
+//     document.getElementById("output").innerHTML= "la parola più corta è : " + str2;
 
-}
-else if(str1.length < str2.length){
-        console.log("la parola piu corta è : " + str1);
-        document.getElementById("output").innerHTML= "la parola più corta è : " + str1;
+// }
+// else if(str1.length < str2.length){
+//         console.log("la parola piu corta è : " + str1);
+//         document.getElementById("output").innerHTML= "la parola più corta è : " + str1;
 
-    }
-    else{
-        console.log("le due parole : " + str1 +" e  " + str2 + "hanno la stessa lunghezza");
-        document.getElementById("output").innerHTML="le due parole : " + str1 +" e " + str2 + " hanno la stessa lunghezza";
-    }
+//     }
+//     else{
+//         console.log("le due parole : " + str1 +" e  " + str2 + "hanno la stessa lunghezza");
+//         document.getElementById("output").innerHTML="le due parole : " + str1 +" e " + str2 + " hanno la stessa lunghezza";
+//     }
 
 
 
 //---------------ES 2------------------------
 
-var numbers = [];
-for(let i=0 ; i<6 ; i++){
-    numbers[i]=Math.floor(prompt("inserisci un numero : "));
-}
-console.log(numbers);
+// var numbers = [];
+// for(let i=0 ; i<6 ; i++){
+//     numbers[i]=Math.floor(prompt("inserisci un numero : "));
+// }
+// console.log(numbers);
 
 var numero=0;
 var somma=0;
-var i=0;
 
-for(i=0 ; i<5 ; i++){
+for(let i=0 ; i<5 ; i++){
     numero = Math.floor(prompt("inserisci un numero : "));
     somma += numero;
     if(numero % 2 == 0){
