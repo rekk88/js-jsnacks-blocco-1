c2a846ccc0a8f12cee306a386b97829d76e7cecf
maicol <maicol.matteoli@gmail.com>
2021-06-01 14:43:18 +0200

    add: innerHTML output

--- js/script.js
+++ js/script.js
@@ -6,11 +6,15 @@ var str2 = prompt("inseriscine un'altra :  ");
 if(str1.length > str2.length)
 {
     console.log("la parola piu corta è : " + str2);
+    document.getElementById("output").innerHTML= "la parola più corta è : " + str2;
 
 }
 else if(str1.length < str2.length){
         console.log("la parola piu corta è : " + str1);
+        document.getElementById("output").innerHTML= "la parola più corta è : " + str1;
+
     }
     else{
         console.log("le due parole : " + str1 +" e  " + str2 + "hanno la stessa lunghezza");
+        document.getElementById("output").innerHTML="le due parole : " + str1 +" e " + str2 + " hanno la stessa lunghezza";
     }
